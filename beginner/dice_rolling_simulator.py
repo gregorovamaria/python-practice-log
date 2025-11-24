f071e265fc9445294f296e6d62470b6ba4098b9f
Maria Gregorova <gregorova.m.maria@gmail.com>
2025-11-24 21:56:47 +0100

    Adding dice rolling simulator

---
+++ beginner/dice_rolling_simulator.py
@@ -0,0 +1,7 @@
+import random
+
+# Select randomly number between 1 and 6
+random_number = random.randrange(1, 7)
+
+# Print outcome of the rolling dice to terminal
+print(f"You've rolled a {random_number}!")
