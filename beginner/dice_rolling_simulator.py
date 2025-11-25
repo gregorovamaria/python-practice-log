7817333405aa08a9de993984a5dc9cd75de0ae96
Maria Gregorova <gregorova.m.maria@gmail.com>
2025-11-25 16:11:50 +0100

    Refactoring code

--- beginner/dice_rolling_simulator.py
+++ beginner/dice_rolling_simulator.py
@@ -1,7 +1,14 @@
+"""Simulate rolling a six-sided die and print the result to the terminal."""
+
 import random
 
-# Select randomly number between 1 and 6
-random_number = random.randrange(1, 7)
+
+def roll_dice():
+    """Return a random integer between 1 and 6, inclusive."""
+    return random.randrange(1, 7)
+
+
+random_number = roll_dice()
 
 # Print outcome of the rolling dice to terminal
 print(f"You've rolled a {random_number}!")
